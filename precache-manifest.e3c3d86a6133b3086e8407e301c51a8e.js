6b8d7130e8ae0b7d5aea61f8a5d40d2fbe8e828c
Goga Hayrapetyan <hayrapg@gmail.com>
2020-03-22 19:09:30 -0400

    Updates

---
+++ precache-manifest.e3c3d86a6133b3086e8407e301c51a8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "174ca08ea474bb0179068269f42f3fce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb3f6d66988c48488d7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1803943aaa8fa81fd2df",
+    "url": "/static/js/2.a68c4791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a68c4791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb3f6d66988c48488d7",
+    "url": "/static/js/main.0c2ae68c.chunk.js"
+  },
+  {
+    "revision": "cd451eae5e73d114a676",
+    "url": "/static/js/runtime-main.c2753d7d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
